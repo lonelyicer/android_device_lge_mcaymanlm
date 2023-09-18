7e1f90ed0c6862c104c3583c34352c7ccec06c2e
SomezakiKureha <63995099+lonelyicer@users.noreply.github.com>
2023-09-18 13:06:15 +0800

    Fix PRODUCT_STATIC_BOOT_CONTROL_HAL

--- device.mk
+++ device.mk
@@ -16,16 +16,13 @@ AB_OTA_POSTINSTALL_CONFIG += \
 # Boot control HAL
 PRODUCT_PACKAGES += \
     android.hardware.boot@1.0-impl \
+    android.hardware.boot@1.0-impl.recovery \
+    bootctrl.mt6885 \
+    bootctrl.mt6885.recovery \
     android.hardware.boot@1.0-service
 
 PRODUCT_PACKAGES += \
-    bootctrl.mt6885
-
-PRODUCT_STATIC_BOOT_CONTROL_HAL := \
-    bootctrl.mt6885 \
-    libgptutils \
-    libz \
-    libcutils
+    bootctrl
 
 PRODUCT_PACKAGES += \
     otapreopt_script \
